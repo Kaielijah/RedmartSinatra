49b9eff3a6f491b477452d56b7d25ea41f185779
Yi Ting Seah <k.seahyt@gmail.com>
2016-07-20 18:35:39 +0800

    input bundles into files

---
+++ app.rb
@@ -0,0 +1,39 @@
+class RedmartApp < Sinatra::Base
+  get '/' do
+    erb 'Hello world'
+  end
+
+  # GET /books LIST ALL THE BOOKS
+  get '/users' do
+    # @books BY RIGHT SHOULD TAKE FROM DATABASE
+
+    # @books = ['Harry Potter', 'Hunger Games', 'Lord of the Rings']
+
+    @users = User.all
+
+    erb :'users'
+  end
+
+  # GET /books/1 GET BOOKS WITH ID 1
+  get '/users/:id' do
+    # some code here
+    erb :'users'
+  end
+
+
+
+
+
+
+
+
+
+  # POST /books CREATE NEW BOOKS
+
+  # PUT /books/1 UPDATE BOOK WITH ID 1
+
+  # PATCH /books/3 PARTIALLY UPDATE BOOK WITH ID 3
+
+  # DELETE /books/1 DELETE BOOK WITH ID 1
+
+end
