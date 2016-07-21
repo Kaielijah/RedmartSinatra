d96de5dbc69d488ac581f725fd1592c51ba76533
Yi Ting Seah <k.seahyt@gmail.com>
2016-07-21 09:39:54 +0800

    updated app.rb and included ccnumberin table

--- app.rb
+++ app.rb
@@ -1,25 +1,16 @@
 class RedmartSinatraApp < Sinatra::Base
   get '/' do
-    erb 'Hello world'
+    erb 'System online
   end
 
-  # GET /books LIST ALL THE BOOKS
-  get '/users' do
-    # @books BY RIGHT SHOULD TAKE FROM DATABASE
 
-    # @books = ['Harry Potter', 'Hunger Games', 'Lord of the Rings']
+  get '/users' do
 
     @users = User.all
 
     erb :'users'
   end
 
-  # POST /books CREATE NEW BOOKS
 
-  # PUT /books/1 UPDATE BOOK WITH ID 1
-
-  # PATCH /books/3 PARTIALLY UPDATE BOOK WITH ID 3
-
-  # DELETE /books/1 DELETE BOOK WITH ID 1
 
 end
